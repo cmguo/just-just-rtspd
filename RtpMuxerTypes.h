2eb46f022e580fd7ec6c1cc4124697d76256f06a
isxxguo <isxxguo@pptv.com>
2013-09-23 07:56:23 +0000

    /ppbox/branches/1.2.0 /src/ppbox/ 调整 ClassFactory, UrlSource UrlSink，移动到 util 库 emailto: box-dev
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@7448 b3a1389e-2159-5c43-9038-463bf877d0d9

--- RtpMuxerTypes.h
+++ RtpMuxerTypes.h
@@ -3,7 +3,7 @@
 #ifndef _PPBOX_RTSPD_RTP_MUXER_TYPES_H_
 #define _PPBOX_RTSPD_RTP_MUXER_TYPES_H_
 
-#include <ppbox/common/ClassRegister.h>
+#include <util/tools/ClassRegister.h>
 
 #include "ppbox/rtspd/raw/RtpRawMuxer.h"
 #include "ppbox/rtspd/raw/RtpFormat.h"
